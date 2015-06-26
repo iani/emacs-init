26cf7b550e55a90d359e60c20e232c307e4953b3
neyzen <zannos@gmail.com>
2015-06-26 14:23:46 +0300

    custom hl-line

--- custom.el
+++ custom.el
@@ -451,6 +451,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(hl-line ((t (:background "#002030"))))
  '(org-block-end-line ((t (:background "#3a3a3a" :foreground "dark cyan"))) t)
  '(powerline-active1 ((t (:inherit mode-line :background "gold1" :foreground "dark blue"))))
  '(powerline-active2 ((t (:background "dark cyan" :foreground "white"))))
