d176f7982dafdb883626cf6d7bf891d9fde62a57
Iannis Zannos <zannos@gmail.com>
2015-06-28 19:30:48 +0300

    Comments for init.el

--- init.el
+++ init.el
@@ -1,7 +1,9 @@
+;; load computer-specific customizations:
 (let ((host-file (concat system-name ".el")))
   (if (file-exists-p host-file)
       (load-file host-file)))
 
+;; load generic customization, from org-mode file:
 (org-babel-load-file
  (concat
   (file-name-directory (or load-file-name (buffer-file-name))) "/user/init.org"))
